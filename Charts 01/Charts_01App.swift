84f177cf496e6cc713ff8ac847fe0856ec32cc6c
Raheel Ahmad <ahmadraheel@gmail.com>
2022-08-20 19:24:03 -0700

    Initial Commit

---
+++ Charts 01/Charts_01App.swift
@@ -0,0 +1,17 @@
+//
+//  Charts_01App.swift
+//  Charts 01
+//
+//  Created by Raheel Ahmad on 8/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Charts_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
